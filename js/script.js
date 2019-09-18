ed3cd3104829427329c534323aab65459176800d
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-09-18 10:32:53 +0300

    Added adaptation for mobile devices

--- js/script.js
+++ js/script.js
@@ -1,21 +1,64 @@
 //slick-one
 $('.slider-one').slick({
   arrows: false,
-  dots: true
+  dots: true,
+  responsive: [{
+    breakpoint: 577,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true
+    }
+  }]
 });
 //slick-two
 $('.slider-two').slick({
   arrows: true,
   dots: false,
   slidesToShow: 4,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [{
+    breakpoint: 577,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false
+    }
+  }]
 });
 //slick-three
 $('.slider-three').slick({
   arrows: true,
   dots: false,
   slidesToShow: 5,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [{
+    breakpoint: 577,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false
+    }
+  }]
+});
+//slick-four
+$('.slider-four').slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [{
+    breakpoint: 577,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false
+    }
+  }]
 });
 // mobile menu
 $('.btn_mobile').on('click', function () {
@@ -35,5 +78,11 @@ $('#mMenuClose').on('click', function () {
 $('.header__link-icon').on('click', function () {
   $('.popup').toggle('show');
 });
-
-
+//fixed header
+window.addEventListener('scroll', function () {
+  if (pageYOffset >= 300) {
+    document.querySelector('.header').classList.add('header-sticky');
+  } else {
+    document.querySelector('.header').classList.remove('header-sticky');
+  }
+});
